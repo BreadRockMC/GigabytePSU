83950a4aa56fd97b50eace8692ea5d548ef89af5
KosmX <kosmx.mc@gmail.com>
2021-08-29 22:12:11 +0200

    name, block shape, texture

--- src/main/java/io/github/breadrockmc/gigabyte/gigabytepsu/blocks/PSU.java
+++ src/main/java/io/github/breadrockmc/gigabyte/gigabytepsu/blocks/PSU.java
@@ -2,9 +2,12 @@ package io.github.breadrockmc.gigabyte.gigabytepsu.blocks;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.ShapeContext;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.explosion.Explosion;
 
@@ -35,5 +38,10 @@ public class PSU extends Block {
     @Override
     public int getWeakRedstonePower(BlockState state, BlockView world, BlockPos pos, Direction direction) {
         return 15;
+    }
+
+    @Override
+    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return VoxelShapes.cuboid(0f, 0f, 0f, 1f, 9f/16f, 1f);
     }
 }
